10d5e496d8d467316b88da44db6a541499425bbe
hanxuwu <skunder301@gmail.com>
2018-08-07 11:16:21 +1000

    fixed untracked files2

---
+++ Learning Django2.0/Blog/myblog_env1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\study\vscode\learni~2\learni~1\learni~1.0\blog\myblog_env1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
